ab8e177c801b4ed3d457fc8b3ee1509c0053bb43
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-03-03 07:58:12 +0000

    commit it

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <input type="text" name="Title" id="" />
+    </div>
+  );
+}
+
+export default App;
