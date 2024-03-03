32bbf5fb4da19b702109acbbd5d058d5675116f5
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-03-03 14:49:59 +0000

    fixed theme

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -4,19 +4,9 @@ import { themeContext } from "../Context/themeContext";
 
 function Todo({ todo, onDelete, onUpdate }) {
     const mode = useContext(themeContext);
-    if (mode === "dark") {
+    
         return (
-            <div style={{ "border": "2px solid white", "margin": "20px", "width": "300px", "padding": "13px" }} >
-                <h3>{todo.title}</h3>
-
-                <h5>{todo.desc}</h5>
-                <button onClick={() => onDelete(todo)}>Delete</button>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div style={{ "border": "2px solid black", "margin": "20px", "width": "300px", "padding": "13px" }} >
+            <div style={{ "border": `2px solid ${mode=="dark" ? "black": "white"} `, "margin": "20px", "width": "300px", "padding": "13px" }} >
                 <h3>{todo.title}</h3>
 
                 <h5>{todo.desc}</h5>
@@ -24,7 +14,7 @@ function Todo({ todo, onDelete, onUpdate }) {
                 <button onClick={() => onUpdate(todo)}>Oops! Want to change it</button>
             </div>
         )
-    }
+    
 }
 
 export default Todo;
