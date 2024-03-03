ebbd5e5b202e85eb36f88fca363ade173320454d
Prem Kumar <95973265+IMKnightRider@users.noreply.github.com>
2024-03-03 10:36:31 +0000

    added toggle theme with provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Todo from "./Components/Todo"
 import { useState } from 'react';
+import { themeContext } from "./Context/themeContext";
 
 function App() {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
 
-  const [todos, setTodos] = useState([{
-    title: "I am",
-    desc: "Here"
-  }, {
-    title: "I am Today",
-    desc: "Here is Coming"
-  }])
+  const [todos, setTodos] = useState([])
 
   const addNew = (t, d) => {
     setTodos([...todos, {
@@ -26,24 +20,45 @@ function App() {
   const onDelete = (todo) => {
     console.log("Done")
     console.log(todo)
-    setTodos(todos.filter((e)=>{
+    setTodos(todos.filter((e) => {
       return e !== todo
     }))
   }
+
+
+
+  const [mode, setMode] = useState("light")
+  const toggleMode = () => {
+    setMode((mode === "light") ? "dark" : "light")
+    if (mode === "light") {
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+    }
+    else {
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    }
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h1>This an Simple Todo App</h1>
-      <h2>Enter Title</h2>
-      <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
-      <h2>Enter Desc</h2>
-      <input type='text' value={desc} onChange={(e) => setDesc(e.target.value)} />
-      <button onClick={() => addNew(title, desc)}>Add a New One</button>
-      {
-        todos.map((todo, i) => {
-          return <Todo key={i} todo={todo} onDelete={onDelete} />
-        })
-      }
+      <themeContext.Provider value={mode}>
+
+        <h1>Hello World</h1>
+        <input type='checkbox' onClick={toggleMode} /> <span>Toggle {mode}</span>
+        <h1>This an Simple Todo App</h1>
+        <h2>Enter Title</h2>
+        <input type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+        <h2>Enter Desc</h2>
+        <input type='text' value={desc} onChange={(e) => setDesc(e.target.value)} />
+        <br />
+        <button onClick={() => addNew(title, desc)}>Add a New One</button>
+        {
+          todos.length === 0 ? <h3>No Todos Add For Yourself</h3> :
+          todos.map((todo, i) => {
+            return <Todo key={i} todo={todo} onDelete={onDelete} />
+          })
+        }
+      </themeContext.Provider>
     </div>
   );
 }
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import { useContext } from "react";
+import { themeContext } from "../Context/themeContext";
 
 function Todo({ todo, onDelete }) {
-    return (
-    <div style={{"border":"2px solid black", "margin": "20px" ,"width":"300px", "padding": "13px"}} >
-        <h3>{todo.title}</h3>
-        <h5>{todo.desc}</h5>
-        <button onClick={()=>onDelete(todo)}>Delete</button>
-        </div>
-    )
+    const mode = useContext(themeContext);
+    if (mode === "dark") {
+        return (
+            <div style={{ "border": "2px solid white", "margin": "20px", "width": "300px", "padding": "13px" }} >
+                <h3>{todo.title}</h3>
+
+                <h5>{todo.desc}</h5>
+                <button onClick={() => onDelete(todo)}>Delete</button>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div style={{ "border": "2px solid black", "margin": "20px", "width": "300px", "padding": "13px" }} >
+                <h3>{todo.title}</h3>
+
+                <h5>{todo.desc}</h5>
+                <button onClick={() => onDelete(todo)}>Delete</button>
+            </div>
+        )
+    }
 }
 
 export default Todo;
